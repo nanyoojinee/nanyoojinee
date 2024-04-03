dd4aa338013c3b51e9e1862b95765fc5a843f000
Yujin Jeong <126642229+nanyoojinee@users.noreply.github.com>
2024-04-03 12:09:00 +0900

    Add files via upload

---
+++ capsule-render-master/model/normalModel/cylinder.js
@@ -0,0 +1,16 @@
+import { checkReversal } from "../../src/util";
+
+const path = height => {
+  height = Number(height);
+  const MAX_WIDTH = 854;
+  const per = height / 2;
+  return `M 61 0 L ${854 - per} 0 A 50 50 0 1 1 ${854 - per} ${height} L ${per} ${height} A 50 50 0 1 1 ${per} 0 `;
+};
+
+const render = (reversal, color, height) => {
+  reversal = checkReversal(reversal);
+
+  return `<path fill="${color}" ${reversal} fill-opacity="1" d="${path(height)}"></path>`;
+};
+
+export default render;
